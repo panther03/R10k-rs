ed4fd6817160fa25a16b9f1c3c144e4235911268
Julien de Castelnau <julien.decastelnau@genusplc.com>
2022-11-08 16:28:09 -0600

    init some data structures

--- ada/r10k/src/r10k.adb
+++ ada/r10k/src/r10k.adb
@@ -1,4 +1,26 @@
+With Ada.Text_IO; Use Ada.Text_IO;
+
 procedure R10k is
+   type PReg is record
+      num: Natural;
+      ready: Boolean;
+   end record;
+
+   function Print_PReg (P: PReg) return String is
+   begin
+      return "PR#" & Integer'Image(P.num) & (if P.ready then "+" else " ");
+   end;
+
+   type VRegType is (F, R);
+
+   type VReg is record
+      type : VRegType;
+      num: Natural range 0 .. 4;
+   end record;
+
+
+
+   P : PReg := (3, true);
 begin
-   null;
+   Ada.Text_IO.Put(Print_PReg(P));
 end R10k;
